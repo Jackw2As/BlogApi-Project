2f8ffe156ae158674d0fa383e19e61a7077bff98
Jackw2As <aaldersjack@gmail.com>
2022-06-19 10:29:09 +0100

    Created a generic Storage Project to handle any concern related to storage of Documents and Database Files.

---
+++ Storage/DatabaseModels/BaseDatabaseModel.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace Storage.DatabaseModels
+{
+    public class BaseDatabaseModel : BaseModel
+    {
+        [Key]
+        public override Guid ID { get => base.ID ; init => base.ID = value; }
+    }
+}
---
+++ Storage/Service/InMemoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Interface;
+using Storage.DatabaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Service
+{
+    internal class InMemoryRepository<T> : IRepository<T> where T : BaseDatabaseModel
+    {
+        public void Create(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Read(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> ReadMultiple()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
