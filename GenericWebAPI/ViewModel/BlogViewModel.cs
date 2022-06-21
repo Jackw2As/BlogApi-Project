951a4c0a253d4327497b97d104cda7bc8c64e202
Jackw2As <aaldersjack@gmail.com>
2022-06-21 09:03:02 +0100

    Added properties to Blog and Post ViewModels. Created a PostDisplayModel specifically for a Summary View.

--- GenericWebAPI/ViewModel/BlogViewModel.cs
+++ GenericWebAPI/ViewModel/BlogViewModel.cs
@@ -4,6 +4,8 @@ namespace Application.ViewModel
 {
     public class BlogViewModel : BaseModel
     {
-
+        public List<PostDisplayModel> DisplayModels { get; set; } = new ();
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
---
+++ GenericWebAPI/ViewModel/PostDisplayModel.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Base;
+
+namespace Application.ViewModel
+{
+    public class PostDisplayModel : BaseModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<BaseTag> Tags { get; set; } = new ();
+    }
+}
