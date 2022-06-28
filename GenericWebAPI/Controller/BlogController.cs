cd6629998dd8ed97a287f01a1f7021109485ff9e
Jackw2As <aaldersjack@gmail.com>
2022-06-28 09:36:56 +0100

    Setup correct Authentication.

--- GenericWebAPI/Controller/BlogController.cs
+++ GenericWebAPI/Controller/BlogController.cs
@@ -1,15 +1,24 @@
 ﻿using Domain.Base;
 using Domain.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Storage.DatabaseModels;
 
 namespace Application.Controller
 {
+    [Authorize()]
     public class BlogController : BaseController<BlogModel>
     {
         protected override IRepository<BlogModel> Repository { get; init; }
         public BlogController(IRepository<BlogModel> repository)
         {
             Repository = repository;
+        }
+
+        [AllowAnonymous]
+        public override IActionResult Get(Guid Id)
+        {
+            return base.Get(Id);
         }
     }
 }
--- GenericWebAPI/Controller/PostController.cs
+++ GenericWebAPI/Controller/PostController.cs
@@ -1,15 +1,24 @@
 ﻿using Domain.Base;
 using Domain.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Storage.DatabaseModels;
 
 namespace Application.Controller
 {
+    [Authorize()]
     public class PostController : BaseController<PostModel>
     {
         protected override IRepository<PostModel> Repository { get; init; }
         public PostController(IRepository<PostModel> repository)
         {
             Repository = repository;
+        }
+
+        [AllowAnonymous]
+        public override IActionResult Get(Guid Id)
+        {
+            return base.Get(Id);
         }
     }
 }
