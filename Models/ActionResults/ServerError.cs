844b7300f008a7c67f754c756ecea3a6fae4fb19
Jackw2As <aaldersjack@gmail.com>
2022-06-28 09:38:24 +0100

    Created a Custome IAction Result to handle server errors. Planned usage is if the Database breaks. Hides actual error to user.

---
+++ Models/ActionResults/ServerError.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Domain.ActionResults
+{
+    public class ServerError : IActionResult
+    {
+        private string message;
+        public ServerError(string message)
+        {
+            this.message = message;
+        }
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            response.Content = new StringContent(message);
+
+            return Task.FromResult(response);
+        }
+    }
+}
