cc5cf6364e4deb0969cb6168d035284c4c39a9d2
Jackw2As <aaldersjack@gmail.com>
2022-07-05 13:25:32 +0100

    Setup InMemoryDBContext that can seed a SQLite Database.

--- Storage/InMemory/InMemoryDBContext.cs
+++ Storage/InMemory/InMemoryDBContext.cs
@@ -1,14 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BlogAPI.Storage.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogAPI.Storage.InMemory
 {
     public class InMemoryDBContext : DbContext
     {
+        //Note: For each DBSet create an Array create a public SeedData Class
+
+        public DbSet<DataObject> DataObjects { get; set; }
+
+        public DataObject[]? DataObjectsSeedData { get; init; }
+        public InMemoryDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
@@ -17,6 +23,9 @@ namespace BlogAPI.Storage.InMemory
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            if(DataObjectsSeedData != null)
+            modelBuilder.Entity<DataObject>().HasData(DataObjectsSeedData);
         }
     }
 }
