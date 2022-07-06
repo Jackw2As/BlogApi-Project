3a6639b4bfdb06f24e1a84b4363199042e4592d9
Jackw2As <aaldersjack@gmail.com>
2022-07-06 09:54:31 +0100

    Created a new Database Object sepecifically for testing.

---
+++ Storage/DatabaseModels/MockDatabaseObject.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogAPI.Storage.DatabaseModels;
+
+public class MockDatabaseObject : DataObject
+{
+    public bool ManipulateMe { get; set; }
+
+    public MockDatabaseObject()
+    {
+        ID = Guid.NewGuid();
+
+        ManipulateMe = true;
+    }
+}
