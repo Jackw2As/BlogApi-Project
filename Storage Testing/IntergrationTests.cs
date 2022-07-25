9a251aacaec673f0910452d01bdb002182421235
Jackw2As <aaldersjack@gmail.com>
2022-07-25 13:45:55 +0100

    Fixed an issue with tests not calling correctly.

--- Storage Testing/IntergrationTests.cs
+++ Storage Testing/IntergrationTests.cs
@@ -163,8 +163,8 @@ namespace BlogAPI.Storage.InMemory
             Assert.True(response.IsSuccessStatusCode);
 
             var blogList = await client.GetFromJsonAsync<List<GetBlog>>($"blog/list");
-            var postList = await client.GetFromJsonAsync<List<GetPost>>($"post/list?ID={blog.ID}");
-            var commentList = await client.GetFromJsonAsync<List<GetComment>>($"post/list?ID={post.ID}");
+            var postList = await client.GetFromJsonAsync<List<GetPost>>($"post/list?BlogId={blog.ID}");
+            var commentList = await client.GetFromJsonAsync<List<GetComment>>($"comment/list?PostId={post.ID}");
 
             Assert.DoesNotContain(blog, blogList);
             Assert.Empty(postList);
@@ -208,8 +208,8 @@ namespace BlogAPI.Storage.InMemory
             Assert.True(response.IsSuccessStatusCode);
 
             var blogList = await client.GetFromJsonAsync<List<GetBlog>>($"blog/list");
-            var postList = await client.GetFromJsonAsync<List<GetPost>>($"post/list?ID={blog.ID}");
-            var commentList = await client.GetFromJsonAsync<List<GetComment>>($"post/list?ID={post.ID}");
+            var postList = await client.GetFromJsonAsync<List<GetPost>>($"post/list?BlogId={blog.ID}");
+            var commentList = await client.GetFromJsonAsync<List<GetComment>>($"comment/list?PostId={post.ID}");
 
             Assert.Contains(blog, blogList);
             Assert.DoesNotContain(post, postList);
@@ -253,8 +253,8 @@ namespace BlogAPI.Storage.InMemory
             Assert.True(response.IsSuccessStatusCode);
 
             var blogList = await client.GetFromJsonAsync<List<GetBlog>>($"blog/list");
-            var postList = await client.GetFromJsonAsync<List<GetPost>>($"post/list?ID={blog.ID}");
-            var commentList = await client.GetFromJsonAsync<List<GetComment>>($"post/list?ID={post.ID}");
+            var postList = await client.GetFromJsonAsync<List<GetPost>>($"post/list?BlogId={blog.ID}");
+            var commentList = await client.GetFromJsonAsync<List<GetComment>>($"comment/list?PostId={post.ID}");
 
             Assert.Contains(blog, blogList);
             Assert.Contains(post, postList);
