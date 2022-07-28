aca7fca812cbf197e1d326e57c6200138e1e0347
Jackw2As <aaldersjack@gmail.com>
2022-07-28 15:45:37 +0100

    #7 Fixed wrong test data being provided. (Expected CreatePost got CreateBlog)

--- Storage Testing/Test Data/PostCreateTestData.cs
+++ Storage Testing/Test Data/PostCreateTestData.cs
@@ -14,16 +14,16 @@ public class PostCreateTestData : IEnumerable<object[]>
         yield return new object[] { new CreatePost(Faker.Lorem.Sentence(101), "", (string)null, null) };
 
         //Summary Maximum Length 255
-        yield return new object[] { new CreateBlog("Good Title", Faker.Lorem.Sentence(256)) };
+        yield return new object[] { new CreatePost("Good Title", Faker.Lorem.Sentence(256), "")};
 
         //Summary Minimum Length 1
-        yield return new object[] { new CreateBlog("Good Title", "") };
+        yield return new object[] { new CreatePost("Good Title", "", "") };
 
         //Content can't be null
-        yield return new object[] { new CreateBlog("Good Title", null) };
+        yield return new object[] { new CreatePost("Good Title", null, "") };
 
         //Content Max Length 5000
-        yield return new object[] { new CreateBlog("Good Title", Faker.Lorem.Sentence(5001)) };
+        yield return new object[] { new CreatePost("Good Title", Faker.Lorem.Sentence(5001), "") };
     }
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
